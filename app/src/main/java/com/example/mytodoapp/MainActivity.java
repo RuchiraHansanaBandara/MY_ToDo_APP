6227fea14265a8a229384c5257da53e21fec3946
Ruchira Bandara <ruchirabandara67@gmail.com>
2021-10-14 14:28:24 +0530

    Create Design Parts

--- app/src/main/java/com/example/mytodoapp/MainActivity.java
+++ app/src/main/java/com/example/mytodoapp/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.mytodoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bttnadd;
+    ListView listView;
+    TextView count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bttnadd = findViewById(R.id.add);
+        listView = findViewById(R.id.todoList);
+        count = findViewById(R.id.todocount);
+
+        bttnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),AddTodo.class);
+                startActivity(i);
+            }
+        });
     }
 }
